c85274c00ff52bbe7206cc057670487f39a6c119
Hyungsuk Hong <aanoaa@gmail.com>
2023-08-15 16:07:51 +0900

    add host subcmd

--- cmd/host.go
+++ cmd/host.go
@@ -36,16 +36,16 @@ import (
 	"github.com/aanoaa/sgviz/repo"
 )
 
-// policyCmd represents the policy command.
-var policyCmd = &cobra.Command{
-	Use:   "policy",
-	Short: "Export policies as csv format.",
-	Long: `Export policies as csv format.
+// hostCmd represents the host command.
+var hostCmd = &cobra.Command{
+	Use:   "host",
+	Short: "Export policies as csv format for host.",
+	Long: `Export policies as csv format for host.
 For example:
 
-  $ sgviz policy
-  src,dst,port,protocol
-  foo,bar.example.com,8080,tcp
+  $ sgviz host
+  src,src_addr,dst,dst_addr,port,protocol
+  foo-host01,192.168.0.1,bar-vserver,10.0.0.1,8080,tcp
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if os.Getenv("DEBUG") != "" {
@@ -60,14 +60,12 @@ For example:
 
 		ctx := context.Background()
 		pr := repo.NewPolicyRepo(db)
-		rows, err := pr.ListGroup(ctx)
+		rows, err := pr.ListHost(ctx)
 		if err != nil {
 			return errors.Wrap(err, "list fail")
 		}
 
-		stdout := cmd.OutOrStdout()
-		w := csv.NewWriter(stdout)
-
+		w := csv.NewWriter(cmd.OutOrStdout())
 		for _, row := range rows {
 			if err := w.Write(row.ToStrings()); err != nil {
 				return errors.Wrap(err, "write fail")
@@ -80,5 +78,5 @@ For example:
 }
 
 func init() {
-	rootCmd.AddCommand(policyCmd)
+	rootCmd.AddCommand(hostCmd)
 }
