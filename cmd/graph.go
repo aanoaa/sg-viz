01358e066864055c9ada7449974bb4763451d48b
Hyungsuk Hong <aanoaa@gmail.com>
2023-08-15 11:34:39 +0900

    impl got graph subcmd

--- cmd/graph.go
+++ cmd/graph.go
@@ -22,36 +22,59 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
+	// import sqlite3.
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+
+	sqlite "github.com/aanoaa/sg-viz/internal/db"
+	"github.com/aanoaa/sg-viz/repo"
 )
 
 // graphCmd represents the graph command.
 var graphCmd = &cobra.Command{
 	Use:   "graph",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Print policies as dot format.",
+	Long: `Print policies as dot format.
+For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("graph called")
+  $ sg-viz graph
+  digraph {
+      "foo" -> "bar.example.com" [label="8080"]
+  }
+
+  $ sg-viz graph | dot -Tsvg > example.svg
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		db, err = sqlite.Conn("./sg.db")
+		if err != nil {
+			return errors.Wrap(err, "db conn fail")
+		}
+
+		ctx := context.Background()
+		pr := repo.NewPolicyRepo(db)
+		rows, err := pr.ListGroup(ctx)
+		if err != nil {
+			return errors.Wrap(err, "list fail")
+		}
+
+		b := new(strings.Builder)
+		fmt.Fprintln(b, "digraph {")
+		for _, row := range rows {
+			fmt.Fprintf(b, "  \"%s\" -> \"%s\" [label=\"%d\"] \n", row.Src, row.Dst, row.Port)
+		}
+		fmt.Fprintln(b, "}")
+		stdout := cmd.OutOrStdout()
+		fmt.Fprintln(stdout, b.String())
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(graphCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// graphCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// graphCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
