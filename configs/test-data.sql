8ddaeb0c6f9cddd6250db610f021e3f161adefa5
Hyungsuk Hong <aanoaa@gmail.com>
2023-09-01 11:54:53 +0900

    ignore test data

--- configs/test-data.sql
+++ configs/test-data.sql
@@ -1,16 +1,16 @@
-INSERT INTO `host` (`hostname`, `ipaddr`) VALUES
-  ("foo-host01", "192.168.0.1"),
-  ("foo-host02", "192.168.0.2"),
-  ("bar.example.com", "");
+-- INSERT INTO `host` (`hostname`, `ipaddr`) VALUES
+--   ("foo-host01", "192.168.0.1"),
+--   ("foo-host02", "192.168.0.2"),
+--   ("bar.example.com", "");
 
-INSERT INTO `sgroup` (`name`) VALUES
-  ("foo"),
-  ("bar.example.com");
+-- INSERT INTO `sgroup` (`name`) VALUES
+--   ("foo"),
+--   ("bar.example.com");
 
-INSERT INTO `host_sgroup` (`host_id`, `sgroup_id`) VALUES
-  (1, 1),
-  (2, 1),
-  (3, 2);
+-- INSERT INTO `host_sgroup` (`host_id`, `sgroup_id`) VALUES
+--   (1, 1),
+--   (2, 1),
+--   (3, 2);
 
-INSERT INTO `policy` (`src`, `dst`, `port`) VALUES
-  (1, 2, 8080);
+-- INSERT INTO `policy` (`src`, `dst`, `port`) VALUES
+--   (1, 2, 8080);
